a82d1978398c5b715dac39376c7ab2922feb30af
DellCrow <52834918+DellCrow@users.noreply.github.com>
2020-12-02 23:36:43 -0300

    Add files via upload

---
+++ models/PessoasModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "pessoas".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $email
+ * @property int $idade
+ */
+class PessoasModel extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'pessoas';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'email', 'idade'], 'required'],
+            [['idade'], 'integer'],
+            [['nome', 'email'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'email' => 'Email',
+            'idade' => 'Idade',
+        ];
+    }
+}
